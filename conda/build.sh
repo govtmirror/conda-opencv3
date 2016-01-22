e78761f085185e213bbd29f4caa2e00864dcc5db
jay <jlaura@usgs.gov>
2016-01-22 06:33:19 -0700

    Trying to rollback to the correct menpo commit

--- conda/build.sh
+++ conda/build.sh
@@ -7,14 +7,12 @@ CMAKE_ARCH="-m"$ARCH
 SHORT_OS_STR=$(uname -s)
 
 if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
+    export CFLAGS="$CFLAGS $CMAKE_ARCH"
+    export LDFLAGS="$LDFLAGS $CMAKE_ARCH"
     DYNAMIC_EXT="so"
     TBB=""
     OPENMP="-DWITH_OPENMP=1"
     IS_OSX=0
-    # There's a bug with CMake at the moment whereby it can't download
-    # using HTTPS - so we use curl to download the IPP library
-    mkdir -p $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e
-    curl -L https://raw.githubusercontent.com/Itseez/opencv_3rdparty/81a676001ca8075ada498583e4166079e5744668/ippicv/ippicv_linux_20151201.tgz -o $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e/ippicv_linux_20151201.tgz
 fi
 if [ "${SHORT_OS_STR}" == "Darwin" ]; then
     IS_OSX=1
@@ -32,16 +30,25 @@ if [ "${SHORT_OS_STR}" == "Darwin" ]; then
 fi
 
 if [ $PY3K -eq 1 ]; then
-    PY_VER_M="${PY_VER}m"
+    PY_VER_M="3.4m"
     OCV_PYTHON="-DBUILD_opencv_python3=1 -DPYTHON3_EXECUTABLE=$PYTHON -DPYTHON3_INCLUDE_DIR=$PREFIX/include/python${PY_VER_M} -DPYTHON3_LIBRARY=${PREFIX}/lib/libpython${PY_VER_M}.${DYNAMIC_EXT}"
 else
     OCV_PYTHON="-DBUILD_opencv_python2=1 -DPYTHON2_EXECUTABLE=$PYTHON -DPYTHON2_INCLUDE_DIR=$PREFIX/include/python${PY_VER} -DPYTHON2_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.${DYNAMIC_EXT} -DPYTHON_INCLUDE_DIR2=$PREFIX/include/python${PY_VER}"
 fi
 
-git clone https://github.com/Itseez/opencv_contrib
-cd opencv_contrib
-git checkout tags/$PKG_VERSION
-cd ..
+export CFLAGS="-I$PREFIX/include -fPIC $CFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+
+# wget the contrib package
+wget -O contrib.tar.gz https://github.com/Itseez/opencv_contrib/archive/3.0.0.tar.gz
+# Copy a cached version for speed
+#cp ../../../src_cache/contrib.tar.gz contrib.tar.gz
+if [ $(shasum -a 256 "contrib.tar.gz") != "8fa18564447a821318e890c7814a262506dd72aaf7721c5afcf733e413d2e12b" ]; then
+    exit 1
+fi
+tar -xzf contrib.tar.gz
+# Seems to be some error with a pow overload - so I just patch it here.
+patch -p0 < $RECIPE_DIR/lsddetector_pow.patch
 
 cmake .. -G"$CMAKE_GENERATOR"                                            \
     $TBB                                                                 \
@@ -61,11 +68,45 @@ cmake .. -G"$CMAKE_GENERATOR"                                            \
     -DWITH_OPENCL=0                                                      \
     -DWITH_OPENNI=0                                                      \
     -DWITH_FFMPEG=0                                                      \
-    -DWITH_VTK=0                                                         \
-    -DINSTALL_C_EXAMPLES=0                                               \
-    -DOPENCV_EXTRA_MODULES_PATH="opencv_contrib/modules"                 \
-    -DCMAKE_SKIP_RPATH:bool=ON                                           \
+    -DOPENCV_EXTRA_MODULES_PATH="opencv_contrib-3.0.0/modules"           \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j${CPU_COUNT}
 make install
 
+
+if [ $IS_OSX -eq 1 ]; then
+    # Fix the lib prefix for the libraries
+    # Borrowed from
+    # http://answers.opencv.org/question/4134/cmake-install_name_tool-absolute-path-for-library-on-mac-osx/
+    CV_LIB_PATH=$PREFIX/lib
+    find ${CV_LIB_PATH} -type f -name "libopencv*.dylib" -print0 | while IFS="" read -r -d "" dylibpath; do
+        echo install_name_tool -id "$dylibpath" "$dylibpath"
+        install_name_tool -id "$dylibpath" "$dylibpath"
+        otool -L $dylibpath | grep libopencv | tr -d ':' | while read -a libs ; do
+            [ "${file}" != "${libs[0]}" ] && install_name_tool -change ${libs[0]} `basename ${libs[0]}` $dylibpath
+        done
+    done
+
+    # Fix the lib prefix for the cv2.so
+    # Borrowed from
+    # http://answers.opencv.org/question/4134/cmake-install_name_tool-absolute-path-for-library-on-mac-osx/
+    find ${SP_DIR} -type f -name "cv2.so" -print0 | while IFS="" read -r -d "" dylibpath; do
+    echo install_name_tool -id "$dylibpath" "$dylibpath"
+        install_name_tool -id "$dylibpath" "$dylibpath"
+        otool -L $dylibpath | grep libopencv | tr -d ':' | while read -a libs ; do
+            [ "${file}" != "${libs[0]}" ] && install_name_tool -change ${libs[0]} `basename ${libs[0]}` $dylibpath
+        done
+    done
+
+    # Fix the lib prefix for the binaries
+    # Borrowed from
+    # http://answers.opencv.org/question/4134/cmake-install_name_tool-absolute-path-for-library-on-mac-osx/
+    CV_BIN_PATH=$PREFIX/bin
+    find ${CV_BIN_PATH} -type f -name "opencv*" -print0 | while IFS="" read -r -d "" dylibpath; do
+        echo install_name_tool -id "$dylibpath" "$dylibpath"
+        install_name_tool -id "$dylibpath" "$dylibpath"
+        otool -L $dylibpath | grep libopencv | tr -d ':' | while read -a libs ; do
+            [ "${file}" != "${libs[0]}" ] && install_name_tool -change ${libs[0]} `basename ${libs[0]}` $dylibpath
+        done
+    done
+fi
