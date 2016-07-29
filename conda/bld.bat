ab0d1483a91609aae24acd1c550ea750ce6378a1
jlaura <jlaura@asu.edu>
2016-07-29 15:03:17 -0700

    Trying 3.1 direct from git

--- conda/bld.bat
+++ conda/bld.bat
@@ -16,7 +16,7 @@ IF %PY_MAJOR% EQU 3 (GOTO :PY3) else (GOTO :PY2)
 
 :PY3
     REM Get python minor version by running a short script:
-    for /f "delims=" %%A in ('python -c "import sys; print(sys.version_info.minor)"') DO SET PY_MINOR=%%A
+    for /f "delims=" %%A in ('%PREFIX%\python -c "import sys; print(sys.version_info.minor)"') DO SET PY_MINOR=%%A
     GOTO :NOTCPP11
 
 :PY2
@@ -31,40 +31,17 @@ IF %PY_MAJOR% EQU 3 (GOTO :PY3) else (GOTO :PY2)
     GOTO :NOTCPP11
 
 :NOTCPP11
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\binary_descriptor.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\bitops.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\daisy.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\lsddetector_pow.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\saliency.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\seeds.patch"
-    git apply --whitespace=fix -p0 "%RECIPE_DIR%\transientareassegmentationmodule.patch"
+    git apply --whitespace=fix -p0 "%RECIPE_DIR%\kcftracker.patch"
+    git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_beamsearch_decoder.patch"
+    git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_hmm_decoder.patch"
 
 :PYTHON_SETUP
 
 set PY_LIB=python%PY_MAJOR%%PY_MINOR%.lib
 
-:: These are here to map cl.exe version numbers, which we use to figure out which
-::   compiler we are using, and which compiler consumers of Qt need to use, to MSVC
-::   year numbers, which is how qt identifies MSVC versions.
-:: Update this with any new MSVC compiler you might use.
-echo @echo 15=9 2008> msvc_versions.bat
-echo @echo 16=10 2010>> msvc_versions.bat
-echo @echo 19=14 2015>> msvc_versions.bat
-
-for /f "delims=" %%A in ('cl /? 2^>^&1 ^| findstr /C:"Version"') do set "CL_TEXT=%%A"
-FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | findstr /C:"Version %%i" > nul && set VSTRING=%%j && goto FOUND
-EXIT 1
-:FOUND
-
-if "%ARCH%" == "64" (
-   set "VSTRING=%VSTRING%Win64"
-)
-
-call :TRIM VSTRING %VSTRING%
-
 cd build
 
-cmake -LAH -G "Visual Studio %VSTRING%"^
+cmake -LAH -G "NMake Makefiles"^
  -DWITH_EIGEN=ON^
  -DWITH_CUDA=OFF^
  -DWITH_OPENCL=OFF^
@@ -107,9 +84,3 @@ RD /S /Q "%LIBRARY_PREFIX%\x64"
 RD /S /Q "%LIBRARY_PREFIX%\x86"
 RD /S /Q "%SRC_DIR%\opencv_contrib"
 exit 0
-
-:TRIM
-  SetLocal EnableDelayedExpansion
-  set Params=%*
-  for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
-  exit /B
